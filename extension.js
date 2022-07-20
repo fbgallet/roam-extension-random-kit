1f7ea39617ecaf5a293b4feb2d703face632d1bd
Fabrice Gallet <74436347+fbgallet@users.noreply.github.com>
2022-07-20 20:19:12 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,25 +1,34 @@
-if (window.roamjs?.extension?.smartblocks) {
-    registerSbCommand();
-    console.log("command loaded!");
-} else {
-    document.body.addEventListener(
-    'roamjs:smartblocks:loaded',
-    () =>
-        window.roamjs?.extension.smartblocks &&
-        registerSbCommand()
-    );
-    console.log("command loaded after Sb!");
-}
+export default {
+    onload: () => {
+        if (window.roamjs?.extension?.smartblocks) {
+            registerSbCommand();
+            console.log("command loaded!");
+        } else {
+            document.body.addEventListener(
+            'roamjs:smartblocks:loaded',
+            () =>
+                window.roamjs?.extension.smartblocks &&
+                registerSbCommand()
+            );
+            console.log("command loaded after Sb!");
+        }
+        console.log("Random Block extension loaded.");
+    },
+    onunload: () => {
+      console.log("Random Block extension unloaded.");
+    }
+  };
 
 function registerSbCommand() {
     window.roamjs.extension.smartblocks.registerCommand({
     text: 'RANDOMNUMBER',
     handler: (context) => (min,max) => [                 
-        getRandomNumberInRange(min,max)
+        getRandomNumberInRange(min,max).toString()
     ],
     }); 
 }
 
 function getRandomNumberInRange(min,max) {
-  
+    return Math.floor(Math.random() * (max - min + 1)) + parseInt(min);
 }
+
